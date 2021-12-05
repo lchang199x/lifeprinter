eac07d05a10b60b4e1f6cf991acd4ec530085805
lchang199x <lchang199x@gmail.com>
2021-12-05 14:41:53 +0800

    Prepare project structure

---
+++ app/src/main/java/com/tptz/lifeprinter/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tptz.lifeprinter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tptz.lifeprinter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
